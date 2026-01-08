3ee24594f95c3f880a7d7f04154189e4fd594902
Object-ions <moshikoatia@gmail.com>
2026-01-08 00:12:22 -0700

    refactor footer: use dynamic navigation data and clean up styles

--- src/components/layout/FooterCopyrights.js
+++ src/components/layout/FooterCopyrights.js
@@ -1,90 +1,73 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
 import HeaderCTA from './HeaderCTA';
+import { PROJECT_LINKS, LEGAL_LINKS } from '../../data/navigation';
 import '../../styles/components/footer.scss';
 
 const FooterCopyrights = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="site-footer" role="contentinfo">
-      <div className="stripe-bg" />
+      <div className="stripe-bg" aria-hidden="true" />
 
-      {/* 1/4 Brand */}
-      <div className="site-footer_brand">
-        <img src={logo} alt="Switch Case Studio logo" width={'75px'} />
-        <br />
-        <br />
-        <p>
-          © {new Date().getFullYear()} <br /> Switch Case Studio LLC
-        </p>
-      </div>
+      <div className="site-footer__content">
+        {/* Col 1: Brand & Copyright */}
+        <div className="footer-col footer-brand">
+          <img src={logo} alt="Switch Case Studio" className="footer-logo" />
+          <p className="copyright-text">
+            &copy; {currentYear} Switch Case Studio LLC
+          </p>
+        </div>
 
-      {/* 1/4 Site nav (matches header) */}
-      <div className="site-footer_col">
-        <ul className="site-footer_nav">
-          <li>
-            <a href="#services">Services</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <Link to="/pricing">Pricing</Link>
-          </li>
-          <li>
-            <a href="#testimonials">Reviews</a>
-          </li>
+        {/* Col 2: Main Navigation */}
+        <div className="footer-col">
+          <h4>Explore</h4>
+          <ul className="footer-nav">
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <Link to="/pricing/web-development">Pricing</Link>
+            </li>
+            <li>
+              <a href="#testimonials">Reviews</a>
+            </li>
+            <li>
+              <a href="#contact">Contact Us</a>
+            </li>
+          </ul>
+        </div>
 
-          <li>
-            <a href="#contact">Contact Us</a>
-          </li>
-        </ul>
-      </div>
-
-      {/* 1/4 Legal */}
-      <div className="site-footer_col">
-        <ul>
-          {/* Projects + deep links that open the modal */}
-          <li className="has-submenu">
-            <Link to="/projects">Projects</Link>
-            <ul className="site-footer_submenu">
-              <li>
-                <Link to="/projects/zahav-medspa">Zahav Medspa</Link>
+        {/* Col 3: Projects (Dynamic) */}
+        <div className="footer-col">
+          <h4>Projects</h4>
+          <ul className="footer-nav">
+            {PROJECT_LINKS.map((proj) => (
+              <li key={proj.to}>
+                <Link to={proj.to}>{proj.label}</Link>
               </li>
-              <li>
-                <Link to="/projects/prodani-miami">ProDani Miami</Link>
-              </li>
-              <li>
-                <Link to="/projects/creatuwheels">Creatuwheels</Link>
-              </li>
-              <li>
-                <Link to="/projects/maritime">Maritime</Link>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
+            ))}
+          </ul>
+        </div>
 
-      {/* 1/4 Copyright */}
-      <div className="site-footer_copy">
-        <ul>
-          <li>
+        {/* Col 4: Action & Legal (Dynamic) */}
+        <div className="footer-col footer-action">
+          <div className="footer-cta-wrapper">
             <HeaderCTA />
-          </li>
-          <li>
-            {' '}
-            <br />
-            <br />
-          </li>
-          <li>
-            <Link to="/privacy">Privacy Policy</Link>
-          </li>
-          <li>
-            <Link to="/terms">Terms of Use</Link>
-          </li>
-          <li>
-            <Link to="/accessibility">Accessibility Statement</Link>
-          </li>
-        </ul>
+          </div>
+
+          <ul className="footer-nav legal-nav">
+            {LEGAL_LINKS.map((link) => (
+              <li key={link.to}>
+                <Link to={link.to}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </footer>
   );
