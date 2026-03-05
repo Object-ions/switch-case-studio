9b54c9b52adabca8c89d7e06f3b0409bcf421fdd
Object-ions <moshikoatia@gmail.com>
2026-03-04 22:40:12 -0700

    Add StarBorder CTA

--- src/components/layout/HeaderCTA.js
+++ src/components/layout/HeaderCTA.js
@@ -1,17 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import StarBorder from '../ui/StarBorder';
 
 const HeaderCTA = () => {
   return (
-    <a
+    <StarBorder
+      as="a"
       href="https://calendar.app.google/83UCJjis2FHUrr1s6"
       target="_blank"
       rel="noreferrer"
       className="headingCTA"
+      innerClassName="headingCTA__inner"
+      color="#d99cff"
+      speed="3s"
+      thickness={4}
     >
       Book a Free Call{' '}
       <FontAwesomeIcon icon={faArrowRight} style={{ fontSize: '12px' }} />
-    </a>
+    </StarBorder>
   );
 };
 
