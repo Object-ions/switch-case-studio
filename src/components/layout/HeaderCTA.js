24203fd51d7f031e2f4b885967a0a9a6179cd1f0
object-ions <moshikoatia@gmail.com>
2026-05-21 22:51:51 -0700

    Refactor HeaderCTA component to remove StarBorder; update styles in header.scss for improved button appearance

--- src/components/layout/HeaderCTA.js
+++ src/components/layout/HeaderCTA.js
@@ -1,23 +1,18 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import StarBorder from '../ui/StarBorder';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const HeaderCTA = () => {
   return (
-    <StarBorder
+    <a
       as="a"
       href="https://calendar.app.google/83UCJjis2FHUrr1s6"
       target="_blank"
       rel="noreferrer"
       className="headingCTA"
-      innerClassName="headingCTA__inner"
-      color="#d99cff"
-      speed="3s"
-      thickness={4}
     >
-      Book a Free Call{' '}
-      <FontAwesomeIcon icon={faArrowRight} style={{ fontSize: '12px' }} />
-    </StarBorder>
+      Book a Free Call{" "}
+      <FontAwesomeIcon icon={faArrowRight} style={{ fontSize: "12px" }} />
+    </a>
   );
 };
 
